aad16e7f8de04aedee5476919969d48fcebaed02
EgorStogov <egst@rambler.ru>
2016-11-26 21:44:28 +0300

    Arrays4

---
+++ Chapter_001/src/main/java/ru/estogov/Duplicate.java
@@ -0,0 +1,38 @@
+package ru.estogov;
+
+import java.util.Arrays;
+
+/**
+* Class Duplicate решение задачи части 001 урока 5.
+* @author Egor Stogov (egst@rambler.ru)
+* @since 26.11.2016
+* @version 1
+*/
+
+public class Duplicate {
+/**
+* Метод удаления повторений в массиве.
+* @param a - первое слово
+* @param b - второе слово
+* @param c - третье слово
+* @param d - четвертое слово
+* @return some value
+*/
+	public String[] delete(String a, String b, String c, String d) {
+		String[] phrase = new String[] {a, b, c, d};
+		String x;
+		int k = 1;
+		for (int i = 0; i <= phrase.length - k; i++) {
+			for (int j = 0; j <= phrase.length - k; j++) {
+				if (phrase[i].equals(phrase[j]) && (i != j)) {
+					x = phrase[phrase.length - 1];
+					phrase[phrase.length - k] = null;
+					phrase[j] = x;
+					k++;
+					j--;
+				}
+			}
+		}
+	return Arrays.copyOf(phrase, phrase.length - k + 1);
+	}
+}
---
+++ Chapter_001/src/test/java/ru/estogov/DuplicateTest.java
@@ -0,0 +1,24 @@
+package ru.estogov;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+* @author Egor Stogov (egst@rambler.ru)
+* @since 26.11.2016
+* @version 1
+*/
+
+public class DuplicateTest {
+/**
+* Test.
+*/
+	@Test
+	public void whenGiveStringThenGetStringWithoutReiteration() {
+		Duplicate duplicate = new Duplicate();
+		String[] test = new String[] {"1", "2"};
+		assertThat(duplicate.delete("1", "2", "2", "1"), is(test));
+	}
+}

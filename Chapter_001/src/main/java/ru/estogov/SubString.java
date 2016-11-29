45839ba66e122696e61d246949224332bf04b709
EgorStogov <egst@rambler.ru>
2016-11-29 14:34:31 +0300

    erg

---
+++ Chapter_001/src/main/java/ru/estogov/SubString.java
@@ -0,0 +1,20 @@
+package ru.estogov;
+
+/**
+* class SubString решение тестового задания части 001.
+* @author Egor Stogov (egst@rambler.ru)
+* @since 29.11.2016
+* @version 1
+*/
+
+public class SubString {
+/**
+* Метод, проверяющий является ли строка sub подстрокой origin.
+* @param origin - строка
+* @param sub - потенциальная подстрока 
+* @return some value
+*/
+	public boolean contains(String origin, string sub) {
+		String 
+	}
+}
